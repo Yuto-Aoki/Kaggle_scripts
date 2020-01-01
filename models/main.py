3dcf61fc0d387998d14d3e5585864c35ab4aa8ac
Yuto-Aoki <yuuto3039@gmail.com>
2020-01-01 23:27:23 +0900

    Add cohen func

--- models/main.py
+++ models/main.py
@@ -20,3 +20,38 @@ from sklearn.model_selection import KFold, StratifiedKFold
 import gc
 import json
 pd.set_option('display.max_columns', 1000)
+
+def eval_qwk_lgb_regr(y_true, y_pred):
+    """
+    Fast cappa eval function for lgb.
+    """
+    dist = Counter(reduce_train['accuracy_group'])
+    for k in dist:
+        dist[k] /= len(reduce_train)
+    reduce_train['accuracy_group'].hist()
+    
+    acum = 0
+    bound = {}
+    for i in range(3):
+        acum += dist[i]
+        bound[i] = np.percentile(y_pred, acum * 100)
+
+    def classify(x):
+        if x <= bound[0]:
+            return 0
+        elif x <= bound[1]:
+            return 1
+        elif x <= bound[2]:
+            return 2
+        else:
+            return 3
+
+    y_pred = np.array(list(map(classify, y_pred))).reshape(y_true.shape)
+
+    return 'cappa', cohen_kappa_score(y_true, y_pred, weights='quadratic'), True
+
+def cohenkappa(ypred, y):
+    y = y.get_label().astype("int")
+    ypred = ypred.reshape((4, -1)).argmax(axis = 0)
+    loss = cohenkappascore(y, y_pred, weights = 'quadratic')
+    return "cappa", loss, True
